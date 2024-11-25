2d94758958209bfdad7a67b5dc37dab001d3a27e
DeekshithP22-AI <deekshithwork22@gmail.com>
2024-11-25 20:30:00 +0530

    added dvc
    added dvc

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,8 +1,8 @@
-from src.cnnClassifier.constants import *
+from cnnClassifier.constants import *
 import os
 from pathlib import Path
-from src.cnnClassifier.utils.common import read_yaml, create_directories
-from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,
+from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
                                                 TrainingConfig,
@@ -105,7 +105,7 @@ class ConfigurationManager:
 
     def get_validation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
-            path_of_model=Path("artifacts/training/model.h5"),
+            path_of_model=Path("artifacts/training/model.keras"),
             training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
